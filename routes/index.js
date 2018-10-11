3360e220c293c7249ebbb3227b5d6eaf4e4bc24f
surendrap720 <surendrap720@gmail.com>
2018-10-11 21:50:58 +0530

    retrieving products from databse , cmodified the api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var Product = require('../models/product');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Express' });
+
+ Product.find( {},  function(err, docs){
+
+      var productChunks = [];
+      var chunkSize = 3;
+      for(var i=0; i < docs.length; i+= chunkSize){
+          productChunks.push(docs.slice(i, i+chunkSize));
+      }
+        res.render('shop/index', { products: productChunks });
+      
+    });
 });
 
 module.exports = router;
