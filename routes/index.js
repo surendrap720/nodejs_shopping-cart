8e4ac422cb73f66a96cb50136b1f623aa5a1942c
surendrap720 <surendrap720@gmail.com>
2018-10-12 17:45:34 +0530

    csrfProtection added

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var Product = require('../models/product');
-
+var csrf = require('csurf');
 var router = express.Router();
+
+var csrfProtection = csrf();
+router.use(csrfProtection);
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,8 +20,21 @@ router.get('/', function(req, res, next) {
           productChunks.push(docs.slice(i, i+chunkSize));
       }
         res.render('shop/index', { products: productChunks });
-      
+
     });
+
+});
+
+router.get('/user/signup',function(req,res,next){
+
+res.render('user/signup',{csrfToken: req.csrfToken()});
+
+});
+
+router.post('/user/signup',function(req,res,next){
+
+res.redirect('/');
+
 });
 
 module.exports = router;
